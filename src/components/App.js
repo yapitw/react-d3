8de2ad0cfa6dc9db6204ecf688e7f4707c5a4298
yapi <yapi@yapis-iMac.local>
2018-02-16 22:53:05 +0800

    init

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+console.log(process.env);
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>React App</h1>
+        <p>OK! Let's start!! Go Go Go~</p>
+      </div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'public', 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env', 'babel-preset-react'],
+            plugins: [
+              'transform-class-constructor-call',
+              'transform-object-rest-spread'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.s?(c|a)ss$/,
+        use: ExtractTextPlugin.extract([
+          {
+            loader: 'css-loader',
+            options: {
+              minimize: true,
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ])
+      }
+    ]
+  },
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({
+      filename: 'sourcemaps/[file].map',
+      fileContext: 'dist'
+    }),
+    new ExtractTextPlugin('style.css')
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    publicPath: '/dist/',
+    compress: true,
+    port: 8080
+  }
+}
